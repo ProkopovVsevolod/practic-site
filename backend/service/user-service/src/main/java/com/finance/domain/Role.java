419aa85f658cffe0719623899cb5c33308e4c641
satishusk <sanpavkaz@gmail.com>
2023-07-04 16:57:07 +0400

    Added domain classes

---
+++ backend/service/user-service/src/main/java/com/finance/domain/Role.java
@@ -0,0 +1,25 @@
+package com.finance.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public enum Role {
+  USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+  private String name;
+
+  Role(String name) {
+    this.name = name;
+  }
+
+  public static Role byName(String name) {
+    for (Role role : Role.values()) {
+      if (role.name.equals(name)) {
+        return role;
+      }
+    }
+    throw new IllegalArgumentException("Role with name: " + name + " not found");
+  }
+}
