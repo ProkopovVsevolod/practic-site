81fbc91868859b895353de7182aaa714635ca61c
satishusk <sanpavkaz@gmail.com>
2023-07-06 01:02:33 +0400

    Added config to Component scan all exception handlers

---
+++ backend/lib/exception/src/main/java/com/finance/exception/config/ExceptionHandlerConfig.java
@@ -0,0 +1,9 @@
+package com.finance.exception.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ExceptionHandlerConfig {
+}
