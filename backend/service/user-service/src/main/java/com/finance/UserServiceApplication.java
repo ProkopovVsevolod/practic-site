544780450cfc9a56495ad8e2e8469c920133f5e6
satishusk <sanpavkaz@gmail.com>
2023-07-06 01:15:04 +0400

    Added EntityScan and EnableJpaRepositories annotations to user service

--- backend/service/user-service/src/main/java/com/finance/UserServiceApplication.java
+++ backend/service/user-service/src/main/java/com/finance/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.finance.domain")
+@EnableJpaRepositories(basePackages = "com.finance.infrastructure.repository")
 public class UserServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
