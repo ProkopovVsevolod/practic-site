8d4d3708c237f1c71fbee24ac039cfcec2b39256
satishusk <sanpavkaz@gmail.com>
2023-07-07 11:09:31 +0400

    Added extends of DependentByUserCrudService

---
+++ backend/service/budget-service/src/main/java/com/finance/budget/service/impl/ExpenseServiceImpl.java
@@ -0,0 +1,17 @@
+package com.finance.budget.service.impl;
+
+import com.finance.budget.domain.operation.expense.Expense;
+import com.finance.budget.infrastructure.repository.contract.base.DependentByUserRepository;
+import com.finance.budget.infrastructure.repository.contract.base.SessionCaller;
+import com.finance.budget.service.contract.ExpenseService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ExpenseServiceImpl extends DependentByUserCrudService<Expense, Long> implements ExpenseService {
+  public ExpenseServiceImpl(DependentByUserRepository<Expense, Long> dependentByUserRepository,
+                            SessionCaller<Expense> sessionCaller) {
+    super(dependentByUserRepository, sessionCaller);
+  }
+}
