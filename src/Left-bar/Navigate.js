12bebce6607daf4812b32b4e4cffb0d560fb8bb4
ProkopovVsevolod <vaprokopov@mail.ru>
2023-07-07 12:18:42 +0400

    Add Left-bar

---
+++ src/Left-bar/Navigate.js
@@ -0,0 +1,29 @@
+import './Navigate.css';
+
+function Navigate () {
+    return (
+        <nav className="left-bar__navigation">
+            <ul className="left-bar__navigation-list">
+                <li className="left-bar__navigation-list-item">
+                    <img src="../../img/left-bar/main.svg" alt="Главная"/>
+                    <a href="">Dashboard</a>
+                </li>
+                <li className="left-bar__navigation-list-item">
+                    <img src="../../img/left-bar/pay.svg" alt=""/>
+                    <a href="">Payment</a>
+                </li>
+                <li className="left-bar__navigation-list-item">
+                    <img src="../../img/left-bar/settings.svg" alt=""/>
+                    <a href="">Setting</a> 
+                </li>
+                <li className="left-bar__navigation-list-item log-out-img">
+                    <img src="../../img/left-bar/logout.svg" alt=""/>
+                    <a href="">Logout</a> 
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+
+export default Navigate;
