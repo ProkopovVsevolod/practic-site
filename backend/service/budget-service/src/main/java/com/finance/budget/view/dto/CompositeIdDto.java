978dc2dccd9d13bb826585524af9d83e9da090b1
satishusk <sanpavkaz@gmail.com>
2023-07-08 22:31:20 +0400

    Added CompositeIdDto

---
+++ backend/service/budget-service/src/main/java/com/finance/budget/view/dto/CompositeIdDto.java
@@ -0,0 +1,13 @@
+package com.finance.budget.view.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompositeIdDto {
+  private Long id;
+  private Long userId;
+}
